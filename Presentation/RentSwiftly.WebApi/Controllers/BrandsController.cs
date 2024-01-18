ad9cc4b47e4f0c3c1fa56a4822e1e3fc21b881d8
AhmetKucukaydiner <ahmetkucukaydiner@gmail.com>
2024-01-18 23:12:31 +0300

    BrandsController added.

---
+++ Presentation/RentSwiftly.WebApi/Controllers/BrandsController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RentSwiftly.Application.Features.CQRS.Commands.BrandCommands;
+using RentSwiftly.Application.Features.CQRS.Handlers.BrandHandlers;
+using RentSwiftly.Application.Features.CQRS.Queries.BrandQueries;
+
+namespace RentSwiftly.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BrandsController : ControllerBase
+    {
+        private readonly CreateBrandCommandHandler _createBrandCommandHandler;
+        private readonly UpdateBrandCommandHandler _updateBrandCommandHandler;
+        private readonly DeleteBrandCommandHandler _deleteBrandCommandHandler;
+        private readonly GetBrandByIdQueryHandler _getBrandByIdQueryHandler;
+        private readonly GetBrandQueryHandler _getBrandQueryHandler;
+
+        public BrandsController(CreateBrandCommandHandler createBrandCommandHandler, UpdateBrandCommandHandler updateBrandCommandHandler, DeleteBrandCommandHandler deleteBrandCommandHandler, GetBrandByIdQueryHandler getBrandByIdQueryHandler, GetBrandQueryHandler getBrandQueryHandler)
+        {
+            _createBrandCommandHandler = createBrandCommandHandler;
+            _updateBrandCommandHandler = updateBrandCommandHandler;
+            _deleteBrandCommandHandler = deleteBrandCommandHandler;
+            _getBrandByIdQueryHandler = getBrandByIdQueryHandler;
+            _getBrandQueryHandler = getBrandQueryHandler;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateBrand(CreateBrandCommand command)
+        {
+            await _createBrandCommandHandler.Handle(command);
+            return Ok("Yeni marka oluşturuldu.");
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteBrand(int id)
+        {
+            await _deleteBrandCommandHandler.Handle (new DeleteBrandCommand(id));
+            return Ok("Marka silindi.");
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateBrand(UpdateBrandCommand command)
+        {
+            await _updateBrandCommandHandler.Handle(command);
+            return Ok("Marka güncellendi.");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> BrandList()
+        {
+            var values = await _getBrandQueryHandler.Handle();
+            return Ok(values);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetBrand(int id)
+        {
+            var value = await _getBrandByIdQueryHandler.Handle(new GetBrandByIdQuery(id));
+            return Ok(value);
+        }
+    }
+}
