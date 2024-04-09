d7705530ad9a2387ab819394aaacf52e730a3343
Gulsah <gulsahkucukaydiner@gmail.com>
2024-04-09 21:36:07 +0300

    comment entity added.

--- Core/RentSwiftly.Domain/Entities/Blog.cs
+++ Core/RentSwiftly.Domain/Entities/Blog.cs
@@ -18,5 +18,6 @@ namespace RentSwiftly.Domain.Entities
         public Category Category { get; set; }
         public string Description { get; set; }
         public List<TagCloud> TagClouds { get; set; }
+        public List<Comment> Comments { get; set; }
     }
 }
