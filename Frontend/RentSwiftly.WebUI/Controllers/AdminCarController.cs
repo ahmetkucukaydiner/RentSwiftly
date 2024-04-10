901a19ae11408f37bdc22dfad1728da9e8639f72
Gulsah <gulsahkucukaydiner@gmail.com>
2024-04-10 18:05:10 +0300

    Admin theme added and partials created for admin.

---
+++ Frontend/RentSwiftly.WebUI/Controllers/AdminCarController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RentSwiftly.WebUI.Controllers
+{
+    public class AdminCarController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
