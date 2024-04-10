8e26357a820a3bd35cccbf19ab94c04f94da67d4
Gulsah <gulsahkucukaydiner@gmail.com>
2024-04-10 21:22:54 +0300

    CreateCarDto annd ResultBrandDto added.

--- Frontend/RentSwiftly.WebUI/Controllers/AdminCarController.cs
+++ Frontend/RentSwiftly.WebUI/Controllers/AdminCarController.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Newtonsoft.Json;
+using RentSwiftly.Dto.BrandDtos;
 using RentSwiftly.Dto.CarDtos;
+using System.Linq;
 
 namespace RentSwiftly.WebUI.Controllers
 {
@@ -27,8 +30,23 @@ namespace RentSwiftly.WebUI.Controllers
         }
 
         [HttpGet]
-        public IActionResult CreateCar()
+        public async Task<IActionResult> CreateCar()
         {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7000/api/brands");
+            if(responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultBrandDto>>(jsonData);
+                List<SelectListItem> brandValues = (from x in values
+                                                    select new SelectListItem
+                                                    {
+                                                        Text = x.Name,
+                                                        Value = x.BrandID.ToString()
+                                                    }).ToList();
+                ViewBag.BrandValues = brandValues;
+                return View();
+            }
             return View();
         }
     }
