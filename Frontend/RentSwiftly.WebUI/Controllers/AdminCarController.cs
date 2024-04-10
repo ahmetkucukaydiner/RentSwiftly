820bc7e2f93dbab2dbde38413a2e7cacd7e3f8d7
Gulsah <gulsahkucukaydiner@gmail.com>
2024-04-10 21:02:09 +0300

    Navbar and car index added. Create car page created.

--- Frontend/RentSwiftly.WebUI/Controllers/AdminCarController.cs
+++ Frontend/RentSwiftly.WebUI/Controllers/AdminCarController.cs
@@ -1,10 +1,33 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RentSwiftly.Dto.CarDtos;
 
 namespace RentSwiftly.WebUI.Controllers
 {
     public class AdminCarController : Controller
     {
-        public IActionResult Index()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public AdminCarController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7000/api/Cars/GetCarWithBrands/");
+            if(responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultCarWithBrandsDto>>(jsonData);
+                return View(values);
+            }
+            return View();            
+        }
+
+        [HttpGet]
+        public IActionResult CreateCar()
         {
             return View();
         }
