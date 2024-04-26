bd31c95db927651d498ff47c6470a24643f8c94b
Ahmet Küçükaydıner <ahmetkucukaydiner@gmail.com>
2024-04-26 21:56:53 +0300

    RentACarTable added.

--- Infrastructure/RentSwiftly.Persistence/Migrations/20240426184938_mig_add_RentACarTable1.Designer.cs
+++ Infrastructure/RentSwiftly.Persistence/Migrations/20240426184938_mig_add_RentACarTable1.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RentSwiftly.Persistence.Context;
 
@@ -11,9 +12,11 @@ using RentSwiftly.Persistence.Context;
 namespace RentSwiftly.Persistence.Migrations
 {
     [DbContext(typeof(RentSwiftlyContext))]
-    partial class RentSwiftlyContextModelSnapshot : ModelSnapshot
+    [Migration("20240426184938_mig_add_RentACarTable1")]
+    partial class mig_add_RentACarTable1
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
