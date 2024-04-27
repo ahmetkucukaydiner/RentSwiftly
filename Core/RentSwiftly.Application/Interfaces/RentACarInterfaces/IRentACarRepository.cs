2e93769adde3bdc9cd24bf93fb0c8279468770b6
Ahmet Küçükaydıner <ahmetkucukaydiner@gmail.com>
2024-04-27 18:44:51 +0300

    RentACarProcess and Customer tables added. RentACarRepository added.

---
+++ Core/RentSwiftly.Application/Interfaces/RentACarInterfaces/IRentACarRepository.cs
@@ -0,0 +1,15 @@
+﻿using RentSwiftly.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentSwiftly.Application.Interfaces.RentACarInterfaces
+{
+    public interface IRentACarRepository
+    {
+        List<RentACar> GetByFilterAsync(Expression<Func<RentACar, bool>> filter);
+    }
+}
---
+++ Infrastructure/RentSwiftly.Persistence/Repositories/RentACarRepositories/RentACarRepository.cs
@@ -0,0 +1,28 @@
+﻿using RentSwiftly.Application.Interfaces.RentACarInterfaces;
+using RentSwiftly.Domain.Entities;
+using RentSwiftly.Persistence.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentSwiftly.Persistence.Repositories.RentACarRepositories
+{
+    public class RentACarRepository : IRentACarRepository
+    {
+        private readonly RentSwiftlyContext _context;
+
+        public RentACarRepository(RentSwiftlyContext context)
+        {
+            _context = context;
+        }
+
+        public List<RentACar> GetByFilterAsync(Expression<Func<RentACar, bool>> filter)
+        {
+            var values = _context.RentACars.Where(filter);
+            return values.ToList();
+        }
+    }
+}
