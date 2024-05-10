c3313e47db694b658b16681db504f14092d6452c
Ahmet Küçükaydıner <ahmetkucukaydiner@gmail.com>
2024-05-10 22:02:37 +0300

    Main page edited.

--- Frontend/RentSwiftly.WebUI/ViewComponents/DefaultViewComponents/_DefaultStatisticsComponentPartial.cs
+++ Frontend/RentSwiftly.WebUI/ViewComponents/DefaultViewComponents/_DefaultStatisticsComponentPartial.cs
@@ -1,11 +1,60 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RentSwiftly.Dto.StatisticsDtos;
 
 namespace RentSwiftly.WebUI.ViewComponents.DefaultViewComponents
 {
     public class _DefaultStatisticsComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _DefaultStatisticsComponentPartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            #region CarCount
+            var client = _httpClientFactory.CreateClient();
+            var responseMessageCarCount = await client.GetAsync("https://localhost:7000/api/Statistics/GetCarCount");
+            if (responseMessageCarCount.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessageCarCount.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<ResultStatisticsDto>(jsonData);
+                ViewBag.CarCount = values.CarCount;
+            }
+            #endregion
+
+            #region LocationCount
+            var responseMessageLocationCount = await client.GetAsync("https://localhost:7000/api/Statistics/GetLocationCount");
+            if (responseMessageLocationCount.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessageLocationCount.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<ResultStatisticsDto>(jsonData);
+                ViewBag.LocationCount = values.LocationCount;
+            }
+            #endregion
+
+            #region BrandCount
+            var responseMessageBrandCount = await client.GetAsync("https://localhost:7000/api/Statistics/GetBrandCount");
+            if (responseMessageBrandCount.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessageBrandCount.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<ResultStatisticsDto>(jsonData);
+                ViewBag.BrandCount = values.BrandCount;
+            }
+            #endregion
+
+            #region CarCountByFuelElectric
+            var responseMessageCarCountByFuelElectric = await client.GetAsync("https://localhost:7000/api/Statistics/GetCarCountByFuelElectric");
+            if (responseMessageCarCountByFuelElectric.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessageCarCountByFuelElectric.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<ResultStatisticsDto>(jsonData);
+                ViewBag.CarCountByFuelElectric = values.CarCountByFuelElectric;
+            }
+            #endregion
+
             return View();
         }
     }
--- Frontend/RentSwiftly.WebUI/ViewComponents/UILayoutViewComponents/_FooterUILayoutComponentPartial.cs
+++ Frontend/RentSwiftly.WebUI/ViewComponents/UILayoutViewComponents/_FooterUILayoutComponentPartial.cs
@@ -1,11 +1,28 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RentSwiftly.Dto.FooterAddressDtos;
 
 namespace RentSwiftly.WebUI.ViewComponents.UILayoutViewComponents
 {
     public class _FooterUILayoutComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _FooterUILayoutComponentPartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7000/api/footeraddresses");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultFooterAddressDto>>(jsonData);
+                return View(values);
+            }
             return View();
         }
     }
