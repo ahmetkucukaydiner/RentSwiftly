5c4a7e35b51b141e5eb524aa719ce19f1dc126bd
Ahmet Küçükaydıner <ahmetkucukaydiner@gmail.com>
2024-05-11 22:54:42 +0300

    Bloglara ait kullanıcı yorumlarının listelenmesi sağlandı.

--- Frontend/RentSwiftly.WebUI/ViewComponents/CommentViewComponents/_CommentListByBlogComponentPartial.cs
+++ Frontend/RentSwiftly.WebUI/ViewComponents/CommentViewComponents/_CommentListByBlogComponentPartial.cs
@@ -1,11 +1,29 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RentSwiftly.Dto.CommentDtos;
 
 namespace RentSwiftly.WebUI.ViewComponents.CommentViewComponents
 {
     public class _CommentListByBlogComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _CommentListByBlogComponentPartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(int id)
+        {
+            ViewBag.BlogId = id;
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync($"https://localhost:7000/api/Comments/CommentListByBlog?id=" + id);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultCommentDto>>(jsonData);
+                return View(values);
+            }
             return View();
         }
     }
