4a8e9d6f20872f338f5d6e53d5206d6a80ee3fc3
Ahmet Küçükaydıner <ahmetkucukaydiner@gmail.com>
2024-05-12 22:46:01 +0300

    AdminCarFeatureDetail added.

--- Core/RentSwiftly.Application/Features/Mediator/Results/CarFeatureResults/GetCarFeatureByCarIdQueryResult.cs
+++ Core/RentSwiftly.Application/Features/Mediator/Results/CarFeatureResults/GetCarFeatureByCarIdQueryResult.cs
@@ -3,7 +3,6 @@
     public class GetCarFeatureByCarIdQueryResult
     {
         public int CarFeatureID { get; set; }
-        public int CarID { get; set; }
         public int FeatureID { get; set; }
         public string FeatureName { get; set; }
         public bool Available { get; set; }
--- Frontend/RentSwiftly.WebUI/Areas/Admin/Controllers/AdminCarFeatureDetailController.cs
+++ Frontend/RentSwiftly.WebUI/Areas/Admin/Controllers/AdminCarFeatureDetailController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RentSwiftly.Dto.CarFeatureDtos;
 
 namespace RentSwiftly.WebUI.Areas.Admin.Controllers
 {
@@ -6,9 +8,24 @@ namespace RentSwiftly.WebUI.Areas.Admin.Controllers
     [Route("Admin/AdminCarFeatureDetail")]
     public class AdminCarFeatureDetailController : Controller
     {
-        [Route("Index/{id}")]
-        public IActionResult Index(int id)
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public AdminCarFeatureDetailController(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        [Route("Index/{id}")]
+        public async Task<IActionResult> Index(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7000/api/CarFeatures?carId=" + id);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultCarFeatureByCarIdDto>>(jsonData);
+                return View(values);
+            }
             return View();
         }
     }
