8906c4e2f49cfa370a57158c97471d1de7b27a4c
Ahmet Küçükaydıner <ahmetkucukaydiner@gmail.com>
2024-05-21 21:32:50 +0300

    CarDetail page added.

--- Frontend/RentSwiftly.WebUI/Controllers/CarController.cs
+++ Frontend/RentSwiftly.WebUI/Controllers/CarController.cs
@@ -14,7 +14,7 @@ namespace RentSwiftly.WebUI.Controllers
             _httpClientFactory = httpClientFactory;
         }
 
-        public async Task<IActionResult> Index()
+		public async Task<IActionResult> Index()
         {
             ViewBag.v1 = "Araçlar";
             ViewBag.v2 = "Aracınızı seçiniz";
@@ -26,6 +26,15 @@ namespace RentSwiftly.WebUI.Controllers
                 var values = JsonConvert.DeserializeObject<List<ResultCarPricingWithCarDto>>(jsonData);
                 return View(values);
             }
+            return View();
+        }
+
+        public async Task<IActionResult> CarDetail(int id)
+        {
+            ViewBag.v1 = "Araç Detayları";
+            ViewBag.v2 = "Aracın Teknik Özellikleri";
+            ViewBag.CarId = id;
+
             return View();
         }
     }
