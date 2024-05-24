eb2adc789479aa914503c94f87f4799fd18287cf
Ahmet Küçükaydıner <ahmetkucukaydiner@gmail.com>
2024-05-25 01:04:04 +0300

    AppRole and AppUser entities added.

---
+++ Core/RentSwiftly.Domain/Entities/AppUser.cs
@@ -0,0 +1,12 @@
+﻿namespace RentSwiftly.Domain.Entities
+{
+	public class AppUser
+	{
+		public int AppUserID { get; set; }
+		public string UserName { get; set; }
+		public string Password { get; set; }
+		public int AppRoleID { get; set; }
+		public AppRole AppRole { get; set; }
+		public string Email { get; set; }
+	}
+}
