1f42fdded8a6418023165ad54863c92537cfd550
Ahmet Küçükaydıner <ahmetkucukaydiner@gmail.com>
2024-05-28 22:15:14 +0300

    Register Controller added.

---
+++ Core/RentSwiftly.Application/Features/Mediator/Handlers/AppUserHandlers/CreateAppUserCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using MediatR;
+using RentSwiftly.Application.Enums;
+using RentSwiftly.Application.Features.Mediator.Commands.AppUserCommands;
+using RentSwiftly.Application.Interfaces;
+using RentSwiftly.Domain.Entities;
+
+namespace RentSwiftly.Application.Features.Mediator.Handlers.AppUserHandlers
+{
+	public class CreateAppUserCommandHandler : IRequestHandler<CreateAppUserCommand>
+	{
+		private readonly IRepository<AppUser> _repository;
+		public CreateAppUserCommandHandler(IRepository<AppUser> repository)
+		{
+			_repository = repository;
+		}
+
+		public async Task Handle(CreateAppUserCommand request, CancellationToken cancellationToken)
+		{
+			await _repository.CreateAsync(new AppUser
+			{
+				Password = request.Password,
+				UserName = request.Username,
+				Email = request.Email,
+				AppRoleID = (int)RolesType.Member
+			});
+		}
+	}
+}
---
+++ Presentation/RentSwiftly.WebApi/Controllers/RegistersController.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using RentSwiftly.Application.Features.Mediator.Commands.AppUserCommands;
+
+namespace RentSwiftly.WebApi.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class RegistersController : ControllerBase
+	{
+		private readonly IMediator _mediator;
+
+		public RegistersController(IMediator mediator)
+		{
+			_mediator = mediator;
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> Register(CreateAppUserCommand command)
+		{
+			await _mediator.Send(command);
+			return Ok("Kullanıcı başarıyla oluşturuldu.");
+		}
+	}
+}
