5cdba5e0fe4aa8784d00af67dc6e5a46239842e7
Ahmet Küçükaydıner <ahmetkucukaydiner@gmail.com>
2024-06-01 21:17:57 +0300

    Login UI added.

---
+++ Frontend/RentSwiftly.WebUI/Controllers/LoginController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RentSwiftly.Dto.LoginDtos;
+
+namespace RentSwiftly.WebUI.Controllers
+{
+    public class LoginController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public LoginController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(CreateLoginDto createLoginDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            return View();
+        }
+    }
+}
